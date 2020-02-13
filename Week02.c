ccb327b4776a33899d2c2263beb166b960082d21
Arreeya <arreeya.su@ku.th>
2020-02-13 22:20:10 +0700

    Note W2

---
+++ Week02.c
@@ -0,0 +1,32 @@
+/* สรุปบรรยายสัปดาห์ที่ 2 เรื่อง " Types, Character, and Expressions " */
+
+#include <stdio.h>
+int main(){
+
+/* // Data Types and Sizes
+printf("sizeof(char)   = %d\n", sizeof(char)); //sizeof(char)   = 1
+printf("sizeof(int)    = %d\n", sizeof(int)); //sizeof(int)    = 4
+printf("sizeof(float)  = %d\n", sizeof(float)); //sizeof(float)  = 4
+printf("sizeof(double) = %d\n", sizeof(double)); //sizeof(double) = 8
+printf("sizeof(short int)  = %d\n", sizeof(short int)); //sizeof(short int)  = 2
+printf("sizeof(long int)   = %d\n", sizeof(long int)); //sizeof(long int)   = 8
+printf("sizeof(unsigned char)  = %d\n", sizeof(unsigned char)); //sizeof(unsigned char)  = 1
+printf("sizeof(unsigned int) = %d\n", sizeof(unsigned int)); //sizeof(unsigned int) = 4
+printf("sizeof(unsigned short int)  = %d\n", sizeof(unsigned short int)); //sizeof(unsigned short int)  = 2
+printf("sizeof(void)   = %d\n", sizeof(void)); //sizeof(void)   = 1 */
+
+/* // Character Input and Output
+char c,name[20];
+c = getchar(); //รับอักขระ 1 ตัวจาก stdin เข้ามาในโปรแกรม
+putchar(c); //ส่งอักขระ 1 ตัวออกไปยัง stdout
+gets(name); //รับอักขระทุกตัว
+fgets(name,20,stdin); //รับอักขระเท่าที่กำหนด
+printf("%s",name); */
+
+/* //Conditional Expression
+int n = 2;
+printf("%d item%s\n", n, n==1 ? "" : "s"); //เงื่อนไขคือ n มีค่าเป็น 1 หรือไม่ หากใช่จะไม่ใส่อะไร หากไม่จะใส่อักษร s */
+
+
+
+}
